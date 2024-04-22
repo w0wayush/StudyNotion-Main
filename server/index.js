19a596c9e8c5f8fabf474b3776378fec4ea9b349
w0wayush <ayupandey82@gmail.com>
2024-04-22 15:54:58 +0530

    Final Commit

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://edtech-studynotion.netlify.app/",
     credentials: true,
   })
 )
